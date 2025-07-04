ad4b2ca5ff0a1156ce09a99d98f9304ce3d7cd11
zabronax <zabronax@gmail.com>
2025-07-04 09:55:54 +0200

    setup initial repository

---
+++ project/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "6.6.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+}
+
+variable "github_token" {
+  description = "The API token for GitHub"
+  type = string
+  sensitive = true
+}
+
+resource "github_repository" "this_repository" {
+  name = "sandefjord-iac-sops"
+  description = "Walktrough of how to use IaC and SOPS for defining projects"
+}
+
+output "project" {
+  value = github_repository.this_repository.html_url
+}
